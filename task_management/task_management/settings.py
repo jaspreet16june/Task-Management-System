8ef7f1f02b9709d5f3e127963b47009a899d379c
Jaspreet <jaspreet.kaur@renewbuy.com>
2025-03-26 11:38:18 +0530

    Added gitignore and updated the requirement.txt file

--- task_management/task_management/settings.py
+++ task_management/task_management/settings.py
@@ -10,17 +10,24 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+import os
 from pathlib import Path
+from dotenv import load_dotenv
 
+# Load environment variables
+load_dotenv()
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = os.getenv("SECRET_KEY")
+
+DEBUG = os.getenv("DEBUG", True)
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent 
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-pyvtqmj2ke$0s+z9=r6_2kcwo_!&=558k)v)t!0t&k2m%p_=p$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
